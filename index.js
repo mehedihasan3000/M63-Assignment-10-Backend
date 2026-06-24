1eab1fe0cee52e2f9377f2788254d44dff0f34bb
mehedihasan3000 <mehedihasan3000@protonmail.com>
2026-06-24 19:53:48 +0600

    get single user, I mean logged in user getting api

--- index.js
+++ index.js
@@ -110,6 +110,15 @@ async function run() {
             res.send(result);
         })
 
+        app.get("/api/logged-user", async (req, res) => {
+            const id = req.query.id;
+            const query = {
+                _id: new ObjectId(id)
+            };
+            const result = await users.findOne(query);
+            res.send(result);
+        })
+
         app.get("/api/all-users", verifyToken, async (req, res) => {
             const cursor = users.find();
             const result = await cursor.toArray();
